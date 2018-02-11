feab5c263623949687982022e03e251b6f6ca62a
Nikita Kodenko <kodenkonikita@gmail.com>
2018-02-11 20:00:57 +0700

    Carthage support added. CocoaPods support removed

---
+++ SAConfettiView/SAConfettiView.h
@@ -0,0 +1,19 @@
+//
+//  SAConfettiView.h
+//  SAConfettiView
+//
+//  Created by nik on 11.02.2018.
+//  Copyright © 2018 SAConfettiView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SAConfettiView.
+FOUNDATION_EXPORT double SAConfettiViewVersionNumber;
+
+//! Project version string for SAConfettiView.
+FOUNDATION_EXPORT const unsigned char SAConfettiViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SAConfettiView/PublicHeader.h>
+
+
